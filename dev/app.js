14d7b445a0fe88c372aabbbcbaeca22b3e738cae
Greg Van Buren <g.vanburen@gmail.com>
2015-03-29 19:01:57 -0700

    Added http service

--- dev/app.js
+++ dev/app.js
@@ -2,25 +2,35 @@ angular.module('app',['ngRoute','ngAnimate'])
 	.config(['$routeProvider', function($routeProvider){
 		$routeProvider
 		.when('/',{
-			templateUrl: './pages/home.html',
+			templateUrl: '../dev/pages/home.html',
 			controller: 'mainCtrl'
 		})
 		.when('/countries',{
-			templateUrl: './pages/countries.html',
+			templateUrl: '../dev/pages/countries.html',
 			controller: 'countryCtrl'
 		})
 		.when('/countries/:country/capital',{
-			templateUrl: './pages/capital.html',
+			templateUrl: '../dev/pages/capital.html',
 			controller: 'capitalCtrl'
-		})
+		});
 	}])
 	.controller('mainCtrl',['$scope', function($scope){
 		$scope.test = "hello";
-		}
-	])
-	// .controller('countryCtrl',['$scope', function($scope){
-
-	// }])
-	// .controller('capital',['$scope', function($scope){
-
-	// }])
+	}])
+	.controller('countryCtrl',['$scope', '$http', function($scope, $http){
+		$http.get('http://api.geonames.org/countryInfoJSON?username=gvanburen')
+		.success(function(data){
+			$scope.countries = data.geonames;
+		})
+		.error(function(){
+			alert("you failed!")
+			console.log("bummer");
+		})
+		$scope.setSelected = function() {
+	        $scope.selected = this.countries;
+	        console.log($scope.selected);
+    	};
+	}])
+	.controller('capital',['$scope', function($scope){
+		$http.get('http://api.geonames.org/searchJSON?q=london&username=gvanburen')
+	}])
